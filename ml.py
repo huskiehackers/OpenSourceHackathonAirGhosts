6f480ffc22d5eeaa3ce8f4def179268be498b5c5
unknown <ethanjrolvera@gmail.com>
2025-10-17 21:24:31 -0500

    More Training Data From Ethan

--- ml.py
+++ ml.py
@@ -8,9 +8,25 @@ divs = ['Hori','Vert','UArr','DArr']
 
 model = tf.keras.models.Sequential([
 
-    tf.keras.layers.Flatten(input_shape=(50, 50)),
-    tf.keras.layers.Dense(128, activation='softmax'),
-    tf.keras.layers.Dense(4)
+        # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(50, 50, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(4, activation='softmax')
     ])
     
 
@@ -33,14 +49,16 @@ train_dataset = train_dataset.map(lambda x, y: (x / 255.0, y))
 test_dataset = test_dataset.map(lambda x, y: (x / 255.0, y))
 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
               metrics=['accuracy'])
               
-model.fit(train_dataset,epochs=10)
+history = model.fit(train_dataset,epochs=15)
 
-test_loss, test_acc = model.evaluate(train_dataset,verbose=2)
+test_loss, test_acc = model.evaluate(test_dataset,verbose=2)
 
 print('\nTest accuracy:', test_acc)
 
 model.save("gdraw.keras")
+
+
 
